27098df9dbb4ed2e9c57e24ec8539e0f5a915c87
ymir <102983464+ymirfritz98@users.noreply.github.com>
2024-11-14 21:27:49 +0200

    a

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -4,7 +4,7 @@ const appError = require("../error/appError");
 const httpStatusText = require("../utils/httpStatusText");
 
 const verifyToken = asyncWrapper(async (req, res, next) => {
-  const token = req.cookies.__vercel_live_token
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vaGFtZWQzNTNAZ21haWwuY29tIiwiX2lkIjoiNjczNjA0ZmYzNmQ1ZTY0OWE2YWJjMTkwIiwicm9sZSI6Ik1BTkFHRVIiLCJpYXQiOjE3MzE2MTI0MzgsImV4cCI6MTczMjIxNzIzOH0.kGsBw0ba_qha9Z1ZEO3I7U517N_WoS3ZYTPdVZZ3Iwc"
 
   if (!token) {
     const error = appError.create(`Please log in to access this resource.${token}`, 401, httpStatusText.ERROR);
