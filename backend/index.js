feaa9ecf5438321dcdade20a2368e5e488374b54
ymir <102983464+ymirfritz98@users.noreply.github.com>
2024-11-14 14:40:04 +0200

    index

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json({ limit: '6mb' })); // Increase as needed
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "localhost:3000",
+  origin: "https://clone-e-commerce-project-backend.vercel.app",
   methods: ["POST", "GET", "DELETE"],
   credentials: true,
 }));
