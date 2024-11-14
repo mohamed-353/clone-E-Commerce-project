63f2ba169f177a2c2eb5b30457769ca01badf850
ymir <102983464+ymirfritz98@users.noreply.github.com>
2024-11-14 14:08:51 +0200

    methods

--- backend/index.js
+++ backend/index.js
@@ -6,51 +6,42 @@ const connectDb = require("./config/db");
 const router = require("./routes/router");
 const httpStatusText = require("./utils/httpStatusText");
 
+
 const app = express();
-app.use(express.json({ limit: '6mb' }));
+app.use(express.json({ limit: '6mb' })); // Increase as needed
 app.use(cookieParser());
-
 app.use(cors({
-  origin: ["http://localhost:3000", "https://clone-e-commerce-project.vercel.app"],
+  origin: process.env.FRONTEND_URL,
+  methods: ["POST", "GET", "DELETE"],
   credentials: true,
 }));
 
-app.options("*", cors({
-  origin: ["http://localhost:3000", "https://clone-e-commerce-project.vercel.app"],
-  credentials: true,
-}));
-
-// Use routes
 app.use("/api", router);
 
 app.use("/home", (req, res) => {
   res.status(200).send("Welcome to the home page");
 });
 
-app.use("/api/allProductsCategory", (req, res) => {
-  // Your logic for handling this route
-  res.status(200).send("All Products Category");
-});
-
-
 connectDb().then(() => {
-  console.log("connected to DB");
+  console.log("connect to DB");
 });
 
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
 });
 
-// Global middleware for not found routes
-app.all("*", (req, res) => {
+// global middleware for not found router
+
+app.all("*", (req, res, next) => {
   return res.status(404).json({
     status: "error",
     message: "this resource is not available",
   });
 });
 
-// Global error handler
+// global error handler
 app.use((error, req, res, next) => {
   return res.json({
     success: false,
